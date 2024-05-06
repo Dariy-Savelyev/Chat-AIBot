d08abc02d586d19f61ec48342b71880c1a8a8e63
Mihail Galay <m.galay@itransition.com>
2024-05-06 20:09:39 +0300

    Create solution architecture

---
+++ ChatBot.Server/ChatBot.Application/MapperProfiles/WeatherForecastProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using ChatBot.Application.Models;
+
+namespace ChatBot.Application.MapperProfiles;
+
+public class WeatherForecastProfile: Profile
+{
+    protected WeatherForecastProfile()
+    {
+        CreateMap<WeatherForecast, WeatherForecast>()
+            .ForMember(
+                dest => dest.Date,
+                opt => opt.MapFrom(s => s.Date))
+            .ForMember(
+                dest => dest.Summary,
+                opt => opt.MapFrom(s => s.Summary))
+            ;
+    }
+}

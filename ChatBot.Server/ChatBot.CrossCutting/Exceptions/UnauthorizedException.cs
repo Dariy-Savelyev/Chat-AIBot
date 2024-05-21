4682d22805b989a12cf594a1855d23acb280e4d2
Mihail Galay <m.galay@itransition.com>
2024-05-21 17:05:57 +0300

    .dockerignore was added

--- ChatBot.Server/ChatBot.CrossCutting/Exceptions/UnauthorizedException.cs
+++ ChatBot.Server/ChatBot.CrossCutting/Exceptions/UnauthorizedException.cs
@@ -2,6 +2,7 @@
 
 namespace ChatBot.CrossCutting.Exceptions;
 
+[Serializable]
 public sealed class UnauthorizedException : BaseException
 {
     public UnauthorizedException(IReadOnlyCollection<ResponseError> errors)

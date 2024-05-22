f83e97ad5dbacbdd750283e062a3905583cd18ea
Dariy-Savelyev <dariy.savelyev@gmail.com>
2024-05-22 17:14:31 +0300

    Test

--- ChatBot.Server/ChatBot.CrossCutting/Extensions/EmailValidator.cs
+++ ChatBot.Server/ChatBot.CrossCutting/Extensions/EmailValidator.cs
@@ -9,11 +9,11 @@ public static class EmailValidator
         var pattern = @"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$";
         var incompletePattern = @"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+$";
 
-        if (Regex.IsMatch(email, pattern))
+        if (Regex.IsMatch(email, pattern, RegexOptions.NonBacktracking, TimeSpan.FromMilliseconds(100)))
         {
             return true;
         }
-        else if (Regex.IsMatch(email, incompletePattern))
+        else if (Regex.IsMatch(email, incompletePattern, RegexOptions.NonBacktracking, TimeSpan.FromMilliseconds(100)))
         {
             return true;
         }

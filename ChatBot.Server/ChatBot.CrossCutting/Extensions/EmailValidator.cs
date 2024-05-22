3ca98d736908ffdc21edd84c136b651be23130f6
Dariy-Savelyev <dariy.savelyev@gmail.com>
2024-05-22 19:28:01 +0300

    GITLAB-5: Edited Extentions

--- ChatBot.Server/ChatBot.CrossCutting/Extensions/EmailValidator.cs
+++ ChatBot.Server/ChatBot.CrossCutting/Extensions/EmailValidator.cs
@@ -6,14 +6,9 @@ public static class EmailValidator
 {
     public static bool IsValidEmail(string email)
     {
-        var pattern = @"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$";
-        var incompletePattern = @"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+$";
+        var pattern = "^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+$";
 
         if (Regex.IsMatch(email, pattern, RegexOptions.NonBacktracking, TimeSpan.FromMilliseconds(100)))
-        {
-            return true;
-        }
-        else if (Regex.IsMatch(email, incompletePattern, RegexOptions.NonBacktracking, TimeSpan.FromMilliseconds(100)))
         {
             return true;
         }
--- ChatBot.Server/ChatBot.CrossCutting/Extensions/PasswordHasher.cs
+++ ChatBot.Server/ChatBot.CrossCutting/Extensions/PasswordHasher.cs
@@ -7,8 +7,8 @@ public static class PasswordHasher
 {
     public static string HashPassword(string password)
     {
-        using var sha256 = SHA256.Create();
-        var hashedBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
+        var passwordBytes = Encoding.UTF8.GetBytes(password);
+        var hashedBytes = SHA256.HashData(passwordBytes);
 
         return Convert.ToBase64String(hashedBytes);
     }

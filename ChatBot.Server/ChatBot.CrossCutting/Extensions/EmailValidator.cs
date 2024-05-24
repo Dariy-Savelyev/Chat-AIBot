a653eaa206f5fdb512478b10be1e5465f5bdad09
Dariy-Savelyev <dariy.savelyev@gmail.com>
2024-05-24 13:51:05 +0300

    GITLAB-5: Edited pattern in EmailValidator

--- ChatBot.Server/ChatBot.CrossCutting/Extensions/EmailValidator.cs
+++ ChatBot.Server/ChatBot.CrossCutting/Extensions/EmailValidator.cs
@@ -6,7 +6,7 @@ public static class EmailValidator
 {
     public static bool IsValidEmail(string email)
     {
-        var pattern = "^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+$";
+        var pattern = @"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$";
 
         if (Regex.IsMatch(email, pattern, RegexOptions.NonBacktracking, TimeSpan.FromMilliseconds(100)))
         {

0bf8010ac3c0fc4e30080c05629ac46533617145
Dariy-Savelyev <dariy.savelyev@gmail.com>
2024-05-27 15:00:20 +0300

    GITLAB-6: Asses custom messages to validation rules

--- ChatBot.Server/ChatBot.Application/Validators/RegistrationValidator.cs
+++ ChatBot.Server/ChatBot.Application/Validators/RegistrationValidator.cs
@@ -12,15 +12,15 @@ public class RegistrationValidator : AbstractValidator<RegistrationModel>
         RuleFor(x => x.UserName)
             .NotEmpty()
             .NotNull()
-            .Must(userRepository.IsUniqueName);
+            .Must(userRepository.IsUniqueName).WithMessage("This name is taken. Please, enter a new name.");
 
         RuleFor(x => x.Email)
             .NotEmpty()
             .NotNull()
-            .Must(EmailValidator.IsValidEmail)
-            .Must(userRepository.IsUniqueEmail);
+            .EmailAddress()
+            .Must(userRepository.IsUniqueEmail).WithMessage("This email is taken. Please, enter a new email.");
 
         RuleFor(x => x.Password)
-            .Equal(x => x.ConfirmPassword);
+            .Equal(x => x.ConfirmPassword).WithMessage("Passwords aren't matching. Please, reenter password.");
     }
 }

bbd31c7c095bb5dc86a43d1872425a74c52d925d
Dariy-Savelyev <dariy.savelyev@gmail.com>
2024-06-29 14:21:06 +0300

    GITLAB-16: Created login form and formatted code

--- chatbot.client/src/App.tsx
+++ chatbot.client/src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter , Routes, Route } from 'react-router-dom';
 import { Registration } from './pages/Registration';
 import { Home } from './pages/Home';
+import { Login } from './pages/Login';
 
 const App: React.FC = () => {
   return (
@@ -9,6 +10,7 @@ const App: React.FC = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="registration" element={<Registration />} />
+        <Route path="login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );

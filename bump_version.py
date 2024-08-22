1a3c7fd733bcd75a53e39ae90119fccad73afb87
Mihail Galay <m.galay@itransition.com>
2024-08-23 00:57:02 +0300

    bump_version.py was updated

--- bump_version.py
+++ bump_version.py
@@ -5,5 +5,15 @@ import sys
 version = sys.argv[1]
 release_type = sys.argv[2]
 
-new_version = semver.VersionInfo.parse(version).bump_release(release_type)
+version_info = semver.VersionInfo.parse(version)
+
+if release_type == 'major':
+    new_version = version_info.bump_major()
+elif release_type == 'minor':
+    new_version = version_info.bump_minor()
+elif release_type == 'patch':
+    new_version = version_info.bump_patch()
+else:
+    raise ValueError(f"Unknown release type: {release_type}")
+
 print(new_version)

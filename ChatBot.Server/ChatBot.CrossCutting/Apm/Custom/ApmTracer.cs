26027a37e588c0749678a3b3243711a1633a98ce
Mihail Galay <m.galay@itransition.com>
2024-08-22 21:06:58 +0300

    ELK Configuration was added

---
+++ ChatBot.Server/ChatBot.CrossCutting/Apm/Custom/ApmTracer.cs
@@ -0,0 +1,37 @@
+﻿using ChatBot.CrossCutting.Apm.Custom.Models;
+using Elastic.Apm.Api;
+
+namespace ChatBot.CrossCutting.Apm.Custom
+{
+    public class ApmTracer(ITracer tracer) : IApmTracer
+    {
+        public ApmTransaction StartTransaction(
+            string name,
+            string type,
+            bool shouldLinkToExisting = true,
+            DistributedTracingData? tracingData = null)
+        {
+            if (shouldLinkToExisting)
+            {
+                var currentTransaction = tracer?.CurrentTransaction;
+                tracingData = currentTransaction?.OutgoingDistributedTracingData;
+            }
+
+            var newTransaction = tracer?.StartTransaction(name, type, tracingData, true);
+
+            return new ApmTransaction(newTransaction);
+        }
+
+        public ApmSpan? StartSpan(string name, string type, string? subType = null)
+        {
+            var currentTransaction = tracer?.CurrentTransaction;
+            if (currentTransaction != null)
+            {
+                var span = currentTransaction.StartSpan(name, type, subType);
+                return new ApmSpan(span);
+            }
+
+            return null;
+        }
+    }
+}
---
+++ ChatBot.Server/ChatBot.CrossCutting/Apm/Custom/Models/ApmTransaction.cs
@@ -0,0 +1,39 @@
+﻿using Elastic.Apm;
+using Elastic.Apm.Api;
+using static ChatBot.CrossCutting.Apm.Shared.Constants;
+
+namespace ChatBot.CrossCutting.Apm.Custom.Models
+{
+    public class ApmTransaction : IDisposable
+    {
+        private readonly ITransaction? _transaction;
+
+        internal ApmTransaction(ITransaction? transaction)
+        {
+            _transaction = transaction;
+        }
+
+        public string TraceId => _transaction?.TraceId ?? string.Empty;
+
+        [Obsolete("IApmTracer should be used instead")]
+        public static ApmTransaction StartNew(string name, string type, bool shouldLinkToExisting = true, DistributedTracingData? tracingData = null)
+        {
+            if (shouldLinkToExisting)
+            {
+                var currentTransaction = Agent.Tracer.CurrentTransaction;
+                tracingData = currentTransaction?.OutgoingDistributedTracingData;
+            }
+
+            var newTransaction = Agent.Tracer.StartTransaction(name, type, tracingData);
+
+            return new ApmTransaction(newTransaction);
+        }
+
+        public void Dispose()
+        {
+            _transaction?.End();
+            _transaction?.Custom.Add(TRANSACTION_HAS_ENDED, string.Empty);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
